47374bf2c69c1d7c1a195507d8bdaf5598808484
SiddPlus <sidd.ghosalkar@outlook.com>
2025-12-02 16:25:49 +0000

    final commit

--- FMP/Source/FMP/FMP.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FMP.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FMP, "FMP" );
-
-DEFINE_LOG_CATEGORY(LogFMP)
